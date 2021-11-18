4479096485afe2b53061bf0a89353e356eaf95be
TheRelow <dev.antropov@gmail.com>
2021-11-18 11:26:12 +0300

    ready

---
+++ src/components/SyncInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <label>
+    sync input
+    <br>
+    <input type="text" v-model="input">
+  </label>
+</template>
+
+<script>
+export default {
+  name: "CustomInput",
+  components: {},
+  data: ()=>({
+    input: ''
+  }),
+  props: [ 'value' ],
+  created() {
+    this.input = this.value
+  },
+  watch: {
+    input: function (val) {
+      this.$emit('update:value', val)
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sync-input v-bind:value.sync="inputData"></sync-input>
+    {{ inputData }}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SyncInput from "../components/SyncInput";
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { SyncInput },
+  data: ()=>({
+    inputData: '123',
+  }),
 }
 </script>
